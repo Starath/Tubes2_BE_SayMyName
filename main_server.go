fc507b0ab72f38d0b3dc2b49a05ea2e762cf8bd7
Starath <19623055@std.stei.itb.ac.id>
2025-05-13 18:06:26 +0700

    Revert "feat: change http request"
    This reverts commit 63bbeb0b28114ca39ca18f5a52fd60e98dc65018.

--- main_server.go
+++ main_server.go
@@ -19,7 +19,7 @@ func Handler(w http.ResponseWriter, r *http.Request) {
 // Middleware CORS
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Access-Control-Allow-Origin", "https://tubes2-fe-say-my-name-yvmx-git-main-rafif-farras-projects.vercel.app/*")
+		w.Header().Set("Access-Control-Allow-Origin", "https://tubes2-fe-say-my-name-yvmx-git-main-rafif-farras-projects.vercel.app")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
